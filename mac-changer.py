52c4f9dd8154d8d1985c8e58b82bda1ff2a5611d
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 15:48:08 +0200

    adding check instance if execution was successful
    + function added to receive current MAC address using Regex
    + printing information on current MAC address before and after execution including a check if script executed correctly

--- mac-changer.py
+++ mac-changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -22,7 +23,25 @@ def change_mac(interface, new_mac):
   subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
   subprocess.call(["ifconfig", interface, "up"])
 
+  
+def get_current_mac(interface):
+  interface_check = subprocess.check_output(["ifconfig", interface])
+  mac_check = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", interface_check)
+  if mac_check:
+    return mac_check.group(0)
+  else:
+    print("Could not read the MAC address.")
+
 
 options = get_arguments()
 
+current_mac = get_current_mac(options.interface) 
+print("Current MAC address: " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+  print("The MAC address was successfully changed to: " + current_mac)
+else:
+  print("The MAC address did not change.")
