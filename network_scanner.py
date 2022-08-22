26373a866d70cbe642c364393be958dba5723e5b
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 19:02:47 +0200

    added initial parsing of information
    + parsing of information
    _ minor clean-up

--- network_scanner.py
+++ network_scanner.py
@@ -8,8 +8,10 @@ def scan(ip):
   arp_request = l2.ARP(pdst=ip)
   broadcast = l2.Ether(dst="ff:ff:ff:ff:ff:ff")
   broadcast_arp_request = broadcast/arp_request
-  answered_list, unanswered_list = scapy.srp(broadcast_arp_request, timeout=1)
-  print(answered_list.summary())
+  answered_list = scapy.srp(broadcast_arp_request, timeout=1)[0]
+  for element in answered_list:
+    print(element)
+    print("---------------")
   
   
 scan("10.0.2.1/24")
