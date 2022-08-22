c9c2d9123ff74f7e1240a04a06ad2ab16ae90f6c
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 15:19:16 +0200

    changing user input to command line arguments
    - user input prompts
    + command line arguments

--- mac-changer.py
+++ mac-changer.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
+parser = optparse.OptionParser()
 
-interface = input("Choose a network interface: ")
-new_mac = input("Choose a new MAC address: ")
+parser.add_option("-i", "--interface", dest="interface", help="Interface you want to change the MAC address for.")
+parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address you want to set.")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("Changing MAC address for " + interface + " to " + new_mac)
 
