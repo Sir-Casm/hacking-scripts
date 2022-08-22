5d6718c3ae96969d1d6626739987bbf8ccef2438
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 18:58:58 +0200

    added sending and receiving functionality
    + sending/receiving functionality

--- network_scanner.py
+++ network_scanner.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import scapy.layers.l2 as l2
+import scapy.all as scapy
 
 
 def scan(ip):
   arp_request = l2.ARP(pdst=ip)
   broadcast = l2.Ether(dst="ff:ff:ff:ff:ff:ff")
   broadcast_arp_request = broadcast/arp_request
-  print(broadcast_arp_request.summary())
-  print(broadcast_arp_request.show())
+  answered_list, unanswered_list = scapy.srp(broadcast_arp_request, timeout=1)
+  print(answered_list.summary())
   
   
 scan("10.0.2.1/24")
