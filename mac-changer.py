8cd7addd70ef4b6c8faab1b5c1fd6f4604908b29
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 15:23:26 +0200

    refactoring: changing MAC as function

--- mac-changer.py
+++ mac-changer.py
@@ -3,6 +3,14 @@
 import subprocess
 import optparse
 
+
+def change_mac(interface, new_mac):
+  print("Changing MAC address for " + interface + " to " + new_mac)
+  subprocess.call(["ifconfig", interface, "down"])
+  subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+  subprocess.call(["ifconfig", interface, "up"])
+
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface you want to change the MAC address for.")
@@ -10,11 +18,4 @@ parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address you w
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("Changing MAC address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
