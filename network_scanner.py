488d66ba00c87e707aeef7e5ba3691f66a93a68c
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 19:24:53 +0200

    added command line input and input validation
    + added command line input function for specifying target during command line execution
    + added input validation for valid IP address or IP address range with Regex

--- network_scanner.py
+++ network_scanner.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+import argparse
+import re
 import scapy.layers.l2 as l2
 import scapy.all as scapy
+
+
+def get_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-t", "--target", dest="target", help="Target you want to scan.")
+  options = parser.parse_args()
+  target_check = re.search(r"^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}($|/(3[0-2]|[1-2][0-9]|[0-9]))$", str(options.target))
+  if not target_check:
+    parser.error("Please specify a valid target IP address or IP range in CIDR notation, use --help or -h for information.")
+  return options
 
 
 def scan(ip):
@@ -22,5 +34,6 @@ def print_result(results_list):
     print(client["ip"] + "\t\t" + client["mac"])
   
   
-scan_result = scan("10.0.2.1/24")
+user_input = get_arguments()
+scan_result = scan(user_input.target)
 print_result(scan_result)
