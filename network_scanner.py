bb1d8cd9a1341dddb537a16f5922023b35d9186b
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 19:15:50 +0200

    separation of scanning and printing function
    + added print_result function
    _ bug-fix on wrong Boolean formatting
    - removed printing overhead in scan() function

--- network_scanner.py
+++ network_scanner.py
@@ -8,10 +8,19 @@ def scan(ip):
   arp_request = l2.ARP(pdst=ip)
   broadcast = l2.Ether(dst="ff:ff:ff:ff:ff:ff")
   broadcast_arp_request = broadcast/arp_request
-  answered_list = scapy.srp(broadcast_arp_request, timeout=1, verbose=false)[0]
-  print("IP\t\t\tMAC address\n-----------------------------------------")
+  answered_list = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
+  clients_list = []
   for element in answered_list:
-    print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+    clients_list.append(clients_dict)
+  return clients_list
+
+
+def print_result(results_list):
+  print("IP\t\t\tMAC address\n-----------------------------------------")
+  for client in results_list:
+    print(client["ip"] + "\t\t" + client["mac"])
   
   
-scan("10.0.2.1/24")
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
