e231c303980b17d545d402c9bb37a5ab34f2b4a7
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 15:34:24 +0200

    further input validation
    + check if user entered any values during execution

--- mac-changer.py
+++ mac-changer.py
@@ -8,7 +8,12 @@ def get_arguments():
   parser = optparse.OptionParser()
   parser.add_option("-i", "--interface", dest="interface", help="Interface you want to change the MAC address for.")
   parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address you want to set.")
-  return parser.parse_args()
+  (options, arguments) = parser.parse_args()
+  if not options.interface:
+    parser.error("Please specify an interface, use --help or -h for information.")
+  if not options.new_mac:
+    parser.error("Please specify a new MAC address, use --help or -h for information.")
+  return options
 
 
 def change_mac(interface, new_mac):
@@ -18,6 +23,6 @@ def change_mac(interface, new_mac):
   subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 
 change_mac(options.interface, options.new_mac)
