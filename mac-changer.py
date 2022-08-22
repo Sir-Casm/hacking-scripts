da4a97eab311f2a8c5ba76e286ad8faf5176c5bb
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 15:26:02 +0200

    refactoring: getting cmd input as function

--- mac-changer.py
+++ mac-changer.py
@@ -4,6 +4,13 @@ import subprocess
 import optparse
 
 
+def get_arguments():
+  parser = optparse.OptionParser()
+  parser.add_option("-i", "--interface", dest="interface", help="Interface you want to change the MAC address for.")
+  parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address you want to set.")
+  return parser.parse_args()
+
+
 def change_mac(interface, new_mac):
   print("Changing MAC address for " + interface + " to " + new_mac)
   subprocess.call(["ifconfig", interface, "down"])
@@ -11,11 +18,6 @@ def change_mac(interface, new_mac):
   subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface you want to change the MAC address for.")
-parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address you want to set.")
-
-(options, arguments) = parser.parse_args()
+(options, arguments) = get_arguments()
 
 change_mac(options.interface, options.new_mac)
