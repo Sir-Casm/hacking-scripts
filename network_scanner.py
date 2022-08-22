a646d063f6b69f3d7e45ce5c929ed9059056df8a
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-22 19:08:32 +0200

    improved presentation of information
    + output now creates a list with IP address(es) and corresponding MAC addresse(es)

--- network_scanner.py
+++ network_scanner.py
@@ -8,11 +8,10 @@ def scan(ip):
   arp_request = l2.ARP(pdst=ip)
   broadcast = l2.Ether(dst="ff:ff:ff:ff:ff:ff")
   broadcast_arp_request = broadcast/arp_request
-  answered_list = scapy.srp(broadcast_arp_request, timeout=1)[0]
+  answered_list = scapy.srp(broadcast_arp_request, timeout=1, verbose=false)[0]
+  print("IP\t\t\tMAC address\n-----------------------------------------")
   for element in answered_list:
-    print(element[1].hwsrc)
-    print(element[1].psrc)
-    print("---------------")
+    print(element[1].psrc + "\t\t" + element[1].hwsrc)
   
   
 scan("10.0.2.1/24")
