0e71222b9a0068dd9f219feb0da8e49f07bb14de
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-26 12:25:37 +0200

    added filter to match http response to http response
    + added list to store the values of the acknowledge field of the HTTP requests
    + added filter to compare the values of the sequence field of the HTTP responses with the list
    - removed unnecessary outputs

--- download_interceptor.py
+++ download_interceptor.py
@@ -5,15 +5,22 @@ import scapy.layers.inet as inet
 import scapy.all as scapy
 
 
+ack_list = []
+
+
 def process_packet(packet):
     scapy_packet = inet.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[inet.TCP].dport == 80:
-            print("HTTP request:")
-            print(scapy_packet.show())
+            if ".zip" in scapy_packet[scapy.Raw].load.decode():
+                print(".zip request")
+                ack_list.append(scapy_packet[inet.TCP].ack)
+                print(scapy_packet.show())
         elif scapy_packet[inet.TCP].sport == 80:
-            print("HTTP response:")
-            print(scapy_packet.show())
+            if scapy_packet[inet.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[inet.TCP].seq)
+                print("Replacing file.")
+                print(scapy_packet.show())
 
     packet.accept()
 
